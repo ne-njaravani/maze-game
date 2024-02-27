0b847b9a971c628d70ef7ee287e04affcd7e6520
Eben <146240499+ne-njaravani@users.noreply.github.com>
2024-02-27 15:55:58 +0000

    Creating some tests with test data and creating the skeleton for the c code

---
+++ maze.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/* 
+Store all maze info
+*/
+typedef struct
+{
+    int start_position[2];
+    int end_position[2];
+    char* maze;
+} MAZE;
+
+typedef struct
+{
+    int current_pos_x;
+    int current_pos_y;
+} PLAYER;
+
+
+
+/* 
+Module: to open the maze file and check whether it was opened properly.
+The method takes in the file name and stores the maze into an array
+// realloc every time or read file twice
+*/
+
+/*
+Function: Check if the maze is valid. Whether:
+-The height and width are in a valid range
+-There is a start point
+-there is and end point
+*/
+
+/*
+Function to update the values of the players current position in the maze
+Parameters: player row position, player column position, the key that was pressed
+*/
+
+int validate_maze()
+{
+    // Check
+    // return 1 if maze is valid
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    // Setup
+
+    // display controls
+
+    // Play
+    // while the player has not made a bad move or has not yet won
+        // player makes a move
+            // Check whether the move that was valid 
+                // if it wasn't then display error message telling the player not to do that
+            // Check if the player reached the exit point
+                // Print winning message if they did & exithw
+    // Exit the code
+    return 0;
+}
---
+++ test.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+gcc maze.c -o maze
+
+echo -e "~~ Testing Maze ~~"
+
+# Make the test script executable
+chmod+x test.sh
+
+# Initialise counters
+all_counter=0
+pass_counter=0
+
+#-------Argument Tests-------
+
+echo -n "Testing no arguments - "
+# Adapted from COMP1921 Lab 2: Testing
+((all_counter++))
+./maze > tmp
+if grep -q "Usage: maze <filename>" tmp;
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+
+echo -n "Testing 1 argument - "
+# Adapted from COMP1921 Lab 2: Testing
+((all_counter++))
+./maze x > tmp
+if grep -q "Usage: maze <filename>" tmp;
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+
+
+#-------FILE TESTS-------
+
+echo -e "\n~~ File Handling Tests~~"
+
+# Test filename
+echo -n "Testing bad filename - "
+# Adapted from COMP1921 Lab 2: Testing
+
+((all_counter++))
+./studentData fake.csv > tmp
+if grep -q "Error: Bad filename" tmp;
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+
+echo -n "Testing whether file exists - "
+# Test whether file exists
+((all_counter++))
+./maze x > tmp
+if grep -q "File Not Found" tmp;
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+# Test whether file is empty
+
+
+# Test execution permissions
+
+# Test whether object file is executable
+
+
+
+echo -e "\n~~ Keypress Tests~~"
+#-------Keypress TESTS-------
+
+echo -n "Testing valid keypress - "
+# Test whether the cases of the valid letters were taken into account
+# Use the read command to read a single character from user input (without echoing it)
+# adapted from a Stack Overflow: shell-script-respond-to-keypress
+# URL: https://stackoverflow.com/questions/24016046/shell-script-respond-to-keypress
+
+((all_counter++))
+read -s -n 1 key
+if [ "$key" == "m" ] || [ "$key" == "M" ] || [ "$key" == "w" ] || [ "$key" == "W" ] || [ "$key" == "a" ] || [ "$key" == "A" ] || [ "$key" == "s" ] || [ "$key" == "S" ] || [ "$key" == "d" ] || [ "$key" == "D" ];
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+
+echo -n "Testing invalid keypress - "
+
+# Test if movement into wall happens
+
+
+# Test if player out of bounds
+
+echo -n "Testing invalid keypress - "
+#Test if map displayed
+
+((all_counter++))
+./maze > tmp
+if grep -q "Here is the Map:" tmp;
+then
+    echo "PASS"
+    ((pass_counter++))
+else
+    echo "FAIL"
+fi
+
+# Display test counter which includes total, passed and percentage passed
+echo "Total tests: $all_counter"
+echo "Passed tests: $pass_counter"
+echo "Percentage passed: $(echo "scale=2; $pass_counter*100 / $all_counter" | bc)%"
+
+# Tidy up and remove the tmp file at the end.
+rm -f tmp
