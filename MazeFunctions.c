0809148cc001814fb7a569d72bc2606eaff14b58
Eben <146240499+ne-njaravani@users.noreply.github.com>
2024-04-20 17:48:58 +0000

    Simplifying the code and adding better functionality

--- MazeFunctions.c
+++ MazeFunctions.c
@@ -19,7 +19,7 @@
  * @param this pointer to the maze to be initialised
  * @param height height to allocate
  * @param width width to allocate
- * @return int 0 on success, 1 on fail
+ * @return int 1 on success, 0 on fail
  */
 int create_maze(maze *this, int height, int width)
 {
@@ -27,7 +27,7 @@ int create_maze(maze *this, int height, int width)
     this->map = malloc(height * sizeof(char *));
     if (this->map == NULL)
     {
-        return 1;
+        return 0;
     }
     // allcocate memory for the width on each row
     for (int i = 0; i < height; i++)
@@ -35,285 +35,308 @@ int create_maze(maze *this, int height, int width)
         this->map[i] = malloc(width * sizeof(char));
         if (this->map[i] == NULL)
         {
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
-    /**
-     * @brief Free the memory allocated to the maze struct
-     *
-     * @param this the pointer to the struct to free
-     */
-    void free_maze(maze * this)
+/**
+ * @brief Free the memory allocated to the maze struct
+ *
+ * @param this the pointer to the struct to free
+ */
+void free_maze(maze *this)
+{
+    // free each row of the map
+    for (int i = 0; i < this->height; i++)
     {
-        // free each row of the map
-        for (int i = 0; i < this->height; i++)
+        free(this->map[i]);
+    }
+}
+
+/**
+ * @brief Validate and return the width of the mazefile
+ *
+ * @param file the file pointer to check
+ * @return int 0 for error, or a valid width (5-100)
+ */
+int get_width(FILE *file)
+{
+    char line[MAX_DIM];
+    if (fgets(line, MAX_DIM, file) == NULL)
+    {
+        return 0;
+    }
+    // Remove newline character from the line length calculation
+    int file_width = strcspn(line, "\n");
+    if (file_width < MIN_DIM)
+    {
+        printf("Invalid maze width: too narrow\n");
+        return 0;
+    }
+    if (file_width > MAX_DIM)
+    {
+        printf("Invalid maze width: too wide\n");
+        return 0;
+    }
+    // Validate: whether each line is the same length as the first line
+    int line_length;
+    while (fgets(line, MAX_DIM, file) != NULL)
+    {
+        line_length = strcspn(line, "\n");
+        if (line_length != file_width)
         {
-            free(this->map[i]);
+            printf("Invalid maze width: inconsistent line length\n");
+            return 0;
         }
     }
+    rewind(file);
+    return file_width;
+}
 
-    /**
-     * @brief Validate and return the width of the mazefile
-     *
-     * @param file the file pointer to check
-     * @return int 0 for error, or a valid width (5-100)
-     */
-    int get_width(FILE * file)
+/**
+ * @brief Validate and return the height of the mazefile
+ *
+ * @param file the file pointer to check
+ * @return int 0 for error, or a valid height (5-100)
+ */
+int get_height(FILE *file)
+{
+    int file_height = 0;
+    char line[MAX_DIM];
+    while (fgets(line, MAX_DIM, file) != NULL)
     {
-        char line[MAX_DIM];
-        if (fgets(line, MAX_DIM, file) == NULL)
+        file_height++;
+    }
+    if (file_height < MIN_DIM)
+    {
+        printf("Invalid maze height: too short\n");
+        return 0;
+    }
+    if (file_height > MAX_DIM)
+    {
+        printf("Invalid maze height: too tall\n");
+        return 0;
+    }
+    rewind(file);
+    return file_height;
+}
+
+/**
+ * @brief read in a maze file into a struct
+ *
+ * @param this Maze struct to be used
+ * @param file Maze file pointer
+ * @return int 1 on success, 0 on fail
+ */
+int read_maze(maze *this, FILE *file)
+{
+    int num_start = 0;
+    int num_end = 0;
+    int i = 0;
+    int j = 0;
+    rewind(file);
+
+    // read the maze into the struct
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        if (ch != '\n')
         {
-            return 0;
-        }
-        // Remove newline character from the line length calculation
-        int file_width = strcspn(line, "\n");
-        if (file_width < MIN_DIM || file_width > MAX_DIM)
-        {
-            return 0;
-        }
-        // Validate: whether each line is the same length as the first line
-        int line_length;
-        while (fgets(line, MAX_DIM, file) != NULL)
-        {
-            line_length = strcspn(line, "\n");
-            if (line_length != file_width)
+            this->map[i][j] = ch;
+
+            // Check if the character is 'S' or 'E'
+            if (ch == 'S')
             {
-                return 0;
+                this->start.x = j;
+                this->start.y = i;
+                num_start++;
+            }
+            if (ch == 'E')
+            {
+                this->end.x = j;
+                this->end.y = i;
+                num_end++;
+            }
+
+            j++;
+            if (j == this->width)
+            {
+                j = 0;
+                i++;
             }
         }
-        rewind(file);
-        return file_width;
     }
 
-    /**
-     * @brief Validate and return the height of the mazefile
-     *
-     * @param file the file pointer to check
-     * @return int 0 for error, or a valid height (5-100)
-     */
-    int get_height(FILE * file)
+    // Validate that there is only one start and end point
+    if (num_start < 1)
     {
-        int file_height = 0;
-        char line[MAX_DIM];
-        while (fgets(line, MAX_DIM, file) != NULL)
-        {
-            file_height++;
-        }
-        if (file_height < MIN_DIM || file_height > MAX_DIM)
-        {
-            return 0;
-        }
-        rewind(file);
-        return file_height;
+        printf("Error: No start point found\n");
+        return 0;
     }
-
-    /**
-     * @brief read in a maze file into a struct
-     *
-     * @param this Maze struct to be used
-     * @param file Maze file pointer
-     * @return int 0 on success, 1 on fail
-     */
-    int read_maze(maze * this, FILE * file)
+    else if (num_start > 1)
     {
-        int num_start = 0;
-        int num_end = 0;
-        int i = 0;
-        int j = 0;
-        rewind(file);
+        printf("Error: More than one start point found\n");
+        return 0;
+    }    
 
-        // read the maze into the struct
-        char ch;
 
-        while ((ch = fgetc(file)) != EOF)
-        {
-            if (ch != '\n')
-            {
-                this->map[i][j] = ch;
-
-                // Check if the character is 'S' or 'E'
-                if (ch == 'S')
-                {
-                    this->start.x = j;
-                    this->start.y = i;
-                    num_start++;
-                }
-                if (ch == 'E')
-                {
-                    this->end.x = j;
-                    this->end.y = i;
-                    num_end++;
-                }
-
-                j++;
-                if (j == this->width)
-                {
-                    j = 0;
-                    i++;
-                }
-            }
-        }
-
-        // Validate that each character in the array is either: ' ', 'S', 'E', '#' or '\n'
-        // if the character is 'S' then increment num_start
-        // if the character is 'E' then increment num_end
-        // if the character is not any of the above then return 1
-        // Validate that there is only one start and one end
-        if (num_start != 1)
-        {
-            return 1;
-        }
-        if (num_end != 1)
-        {
-            return 1;
-        }
-
-        // Validate that the start and end are not the same
-        if (this->start.x == this->end.x && this->start.y == this->end.y)
-        {
-            return 1;
-        }
-
-        fclose(file);
+    if (num_end < 1)
+    {
+        printf("Error: No end point found\n");
+        return 0;
+    }
+    else if (num_end > 1)
+    {
+        printf("Error: More than one end point found\n");
         return 0;
     }
 
-    /**
-     * @brief Set the player's position to the start of the maze
-     *
-     * @param player the player's position
-     * @param this the maze to use
-     */
-    void initialise_player(coord * player, maze * this)
-    {
-        player->x = this->start.x;
-        player->y = this->start.y;
-    }
 
-    /**
-     * @brief Prints the maze out - code provided to ensure correct formatting
-     *
-     * @param this pointer to maze to print
-     * @param player the current player location
-     */
-    void print_maze(maze * this, coord * player)
+
+    fclose(file);
+    return 1;
+}
+
+/**
+ * @brief Set the player's position to the start of the maze
+ *
+ * @param player the player's position
+ * @param this the maze to use
+ */
+void initialise_player(coord *player, maze *this)
+{
+    player->x = this->start.x;
+    player->y = this->start.y;
+}
+
+/**
+ * @brief Prints the maze out - code provided to ensure correct formatting
+ *
+ * @param this pointer to maze to print
+ * @param player the current player location
+ */
+void print_maze(maze *this, coord *player)
+{
+    // make sure we have a leading newline..
+    printf("\n");
+    for (int i = 0; i < this->height; i++)
     {
-        // make sure we have a leading newline..
-        printf("\n");
-        for (int i = 0; i < this->height; i++)
+        for (int j = 0; j < this->width; j++)
         {
-            for (int j = 0; j < this->width; j++)
+            // decide whether player is on this spot or not
+            if (player->x == j && player->y == i)
             {
-                // decide whether player is on this spot or not
-                if (player->x == j && player->y == i)
-                {
-                    printf("X");
-                }
-                else
-                {
-                    printf("%c", this->map[i][j]);
-                }
+                printf("X");
             }
-            // end each row with a newline.
-            printf("\n");
+            else
+            {
+                printf("%c", this->map[i][j]);
+            }
         }
+        // end each row with a newline.
+        printf("\n");
+    }
+}
+
+/**
+ * @brief Validates and makes a movement in a given direction
+ *
+ * @param this Maze struct
+ * @param player The player's current position
+ * @param player_input The desired direction to move in
+ * @return int 0 for invalid move, 1 for valid move
+ */
+int move(maze *this, coord *player, char player_input)
+{
+    int new_x, new_y;
+    char *direction[4] = {"UP", "DOWN", "LEFT", "RIGHT"};
+    player_input = toupper(player_input);
+    char *move;
+
+    switch (player_input)
+    {
+    case 'a':
+    case 'A':
+        new_x = player->x - 1;
+        new_y = player->y;
+        move = direction[2];
+        break;
+
+    case 'd':
+    case 'D':
+        new_x = player->x + 1;
+        new_y = player->y;
+        move = direction[3];
+        break;
+
+    case 'w':
+    case 'W':
+        new_x = player->x;
+        new_y = player->y - 1;
+        move = direction[0];
+        break;
+
+    case 's':
+    case 'S':
+        new_x = player->x;
+        new_y = player->y + 1;
+        move = direction[1];
+        break;
+
+    case 'm':
+    case 'M':
+        printf("\nLoading map...\n");
+        print_maze(this, player);
+        printf("\n");
+        return 1;
+        break;
+
+    default:
+        printf("Whoa! That input was invalid.\nLet's stick to the game's controls, shall we?\n");
+        return 0;
+        break;
     }
 
-    /**
-     * @brief Validates and makes a movement in a given direction
-     *
-     * @param this Maze struct
-     * @param player The player's current position
-     * @param player_input The desired direction to move in
-     * @return int 0 for invalid move, 1 for valid move
-     */
-    int move(maze * this, coord * player, char player_input)
+    // Check if the movement is in the boundaries of the maze
+    if (new_x < 0 || new_x >= this->width || new_y < 0 || new_y >= this->height)
     {
-        int new_x, new_y;
-        char *direction[4] = {"Up", "Down", "Left", "Right"};
-        player_input = toupper(player_input);
+        printf("Hold on, explorer! The secret to solving the maze is staying within its boundaries.\nLet's try another direction!\n");
+        return 0;
+    }
 
-        switch (player_input)
-        {
-        case 'a':
-        case 'A':
-            new_x = player->x - 1;
-            new_y = player->y;
-            printf("You have moved %s.\n", direction[2]);
-            break;
+    if (this->map[new_y][new_x] == '#')
+    {
+        printf("Oops! Trying to phase through walls? You're not The Flash!\nLet's stick to the open paths.\n");
+        return 0;
+    }
+    else
+    {
+        player->x = new_x;
+        player->y = new_y;
+         printf("You have moved %s.\n", move);
+    }
 
-        case 'd':
-        case 'D':
-            new_x = player->x + 1;
-            new_y = player->y;
-            printf("You have moved %s.\n", direction[3]);
-            break;
+    return 1;
+}
 
-        case 'w':
-        case 'W':
-            new_x = player->x;
-            new_y = player->y - 1;
-            printf("You have moved %s.\n", direction[0]);
-            break;
-
-        case 's':
-        case 'S':
-            new_x = player->x;
-            new_y = player->y + 1;
-            printf("You have moved %s.\n", direction[1]);
-            break;
-
-        case 'm':
-        case 'M':
-            printf("\nLoading map...\n");
-            print_maze(this, player);
-            break;
-
-        default:
-            printf("Invalid input\n");
-            return 0;
-            break;
-        }
-
-        // Check if the movement is in the boundaries of the maze
-        if (new_x < 0 || new_x >= this->width || new_y < 0 || new_y >= this->height)
-        {
-            printf("Error. You will go out of the maze bounds. \n");
-            return 0;
-        }
-
-        if (this->map[new_y][new_x] == '#')
-        {
-            printf("Invalid move: You can't move through the walls '#'\n");
-            return 0;
-        }
-        else
-        {
-            player->x = new_x;
-            player->y = new_y;
-        }
-
+/**
+ * @brief Check whether the player has won and return a pseudo-boolean
+ *
+ * @param this current maze
+ * @param player player position
+ * @return int 0 for false, 1 for true
+ */
+int has_won(maze *this, coord *player)
+{
+    if (player->x == this->end.x && player->y == this->end.y)
+    {
         return 1;
     }
-
-    /**
-     * @brief Check whether the player has won and return a pseudo-boolean
-     *
-     * @param this current maze
-     * @param player player position
-     * @return int 0 for false, 1 for true
-     */
-    int has_won(maze * this, coord * player)
+    else
     {
-        if (player->x == this->end.x && player->y == this->end.y && this->map[player->x][player->y] == 'E')
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
+        return 0;
     }
+}
--- maze.c
+++ maze.c
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
     int valid_read, valid_create, valid_move;
     int win = 0;
     char player_move;
-    char welcome_message[] = "\n \\ \\        /  ____|  |       ___|   _ \\    \\  |  ____|      __ __|  _ \\ "
+    char welcome_message[] = "\n\n \\ \\        /  ____|  |       ___|   _ \\    \\  |  ____|      __ __|  _ \\ "
                              "\n  \\ \\  \\   /   __|    |      |      |   |  |\\/ |  __|           |   |   |"
                              "\n   \\ \\  \\ /    |      |      |      |   |  |   |  |             |   |   |"
                              "\n    \\_/\\_/    _____| _____| \\____| \\___/  _|  _| _____|        _|  \\___/ "
@@ -33,20 +33,27 @@ int main(int argc, char const *argv[])
                              "\n\n   ___|     \\      \\  |  ____|  |  |                                     "
                              "\n  |  __    _ \\    |\\/ |  __|    |  |                                     "
                              "\n  |   |   ___ \\   |   |  |     _| _|                                     "
-                             "\n \\____| _/    _\\ _|  _| _____| _) _)                                     ";
+                             "\n \\____| _/    _\\ _|  _| _____| _) _)                                     \n\n";
 
-    char winner_message[] = "\n     \\            \\  |     \\    __  / _ _|   \\  |   ___|  |  |  |       "
-                            "\n    _ \\          |\\/ |    _ \\      /    |     \\ |  |      |  |  |       "
+    char winner_message[] = "\n\n     \\            \\  |     \\    __  / _ _|   \\  |   ___|  |  |  |       "
+                            "\n    _ \\          |\\/ |    _ \\      /    |     \\ |  |  __  |  |  |       "
                             "\n   ___ \\ _____|  |   |   ___ \\    /     |   |\\  |  |   | _| _| _|       "
-                            "\n _/    _\\       _|  _| _/    _\\ ____| ___| _| \\_| \\____| _) _) _)       "
+                            "\n _/    _\\       _|  _| _/    _\\ ____| ___| _| \\_| \\____| _) _) _) (Yes pun intended)      "
                             "\n\n \\ \\   /  _ \\   |   |  ) \\ \\     /  ____|      \\ \\        /  _ \\    \\  |"
                             "\n  \\   /  |   |  |   | /   \\ \\   /   __|         \\ \\  \\   /  |   |    \\ |"
                             "\n     |   |   |  |   |      \\ \\ /    |            \\ \\  \\ /   |   |  |\\  |"
                             "\n    _|  \\___/  \\___/        \\_/    _____|         \\_/\\_/   \\___/  _| \\_|"
                             "\n\n __ __|  |   |  ____|        ___|     \\     \\   |  ____|  |  |          "
-                            "\n    |    |   |  __|         |        _ \\    |\\/ |  __|    |  |          "
+                            "\n    |    |   |  __|         |  __    _ \\    |\\/ |  __|    |  |          "
                             "\n    |    ___ |  |           |   |   ___ \\   |   |  |     _| _|          "
-                            "\n   _|   _|  _| _____|      \\____| _/    _\\ _|  _| _____| _) _)          ";
+                            "\n   _|   _|  _| _____|      \\____| _/    _\\ _|  _| _____| _) _)          \n\n";
+
+    char controls[] = "\n\nReady for an adventure? Here's your control guide:\n"
+                      "\tW - Move up\n"
+                      "\tA - Move left\n"
+                      "\tS - Move down\n"
+                      "\tD - Move right\n"
+                      "\tM - Display the map\n\n";
 
     // Check args
     if (argc != 2)
@@ -61,39 +68,69 @@ int main(int argc, char const *argv[])
         file = open_file(argv[1]);
     }
 
-    printf("%s\n", welcome_message);
-
     // Read in maze file to struct
     this_maze->width = get_width(file);
+
+    if (this_maze->width == 0)
+    {
+        return CODE_MAZE_ERROR;
+    }
+
     this_maze->height = get_height(file);
+
+    if (this_maze->height == 0)
+    {
+        return CODE_MAZE_ERROR;
+    }
 
     valid_create = create_maze(this_maze, this_maze->height, this_maze->width);
 
-    rewind(file);
+    if (valid_create == 0)
+    {
+        printf("Failed to create maze\n");
+        return CODE_MAZE_ERROR;
+    }
+
     valid_read = read_maze(this_maze, file);
+
+    if (valid_read == 0)
+    {
+        return CODE_MAZE_ERROR;
+    }
+
+
+
+    if (valid_read == 0)
+    {
+        printf("Failed to read maze\n");
+        return CODE_MAZE_ERROR;
+    }
+
+    printf("\n%s\n", welcome_message);
+    printf("%s", controls);
 
     initialise_player(player, this_maze);
 
     // Play (maze game loop)
     while (win == 0)
     {
+        printf("\nEnter your move: ");
         scanf(" %c", &player_move);
-        printf("Player entered %c\n", player_move);
         player_move = toupper(player_move);
         valid_move = move(this_maze, player, player_move);
         // Keep asking for a move until a valid move is made
         while (valid_move == 0)
         {
-            printf("Invalid move. Try again.\n");
+            printf("\nEnter your move: \n");
             scanf(" %c", &player_move);
             player_move = toupper(player_move);
             valid_move = move(this_maze, player, player_move);
-            printf("Valid move: %d\n", valid_move);
+            printf("\n");
         }
         win = has_won(this_maze, player);
     }
 
-    printf("A-MAZING!!! You have won the game!\n");
+    printf("\n%s\n", winner_message);
     free_maze(this_maze);
     return CODE_SUCCESS;
     // return, free, exit
